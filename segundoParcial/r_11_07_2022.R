8f923538ba03b2e295c8c20647abe736a5f96590
Jorge Lopez Bauer <jorge.lopezbauer@ing.austral.edu.ar>
2022-11-28 14:39:18 -0300

    r_11_07_2022 1 b

--- segundoParcial/r_11_07_2022.R
+++ segundoParcial/r_11_07_2022.R
@@ -1,5 +1,5 @@
 # 1
-# a
+# a (u y d sabiendo pa y pb)
 a = 3
 b = 6
 pa = 0.25
@@ -8,3 +8,11 @@ qa = qnorm(pa)
 qb = qnorm(pb)
 u = (qa*b-qb*a)/(qa-qb)
 d = (a-u)/qa
+# b (a > b)
+ua = 3.872432
+da = 1.29347
+ub = 3
+db = 0.5
+uDiff = ua - ub
+dDiff = da + db
+p = 1-pnorm(0, uDiff, dDiff)
